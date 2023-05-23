bb611b6890439d16aa96998884811ce8bca3287c
Miguel <miguel.cano.de.sola@gmail.com>
2023-05-23 17:03:33 +0200

    login view apariencia lista

---
+++ lib/constants/colors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const backgroundAppColor = Color(0xFFEEAA95);
+const secondaryAppColor = Color(0xFFD66C4A);
--- lib/views/login.dart
+++ lib/views/login.dart
@@ -1,4 +1,9 @@
+import 'package:country_app/constants/colors.dart';
+import 'package:country_app/icon.dart';
+import 'package:country_app/services/auth/bloc/auth_bloc.dart';
+import 'package:country_app/services/auth/bloc/auth_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -8,8 +13,202 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  late final TextEditingController emailController;
+  late final TextEditingController passController;
+
+  @override
+  void initState() {
+    emailController = TextEditingController();
+    passController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return BlocListener<AuthBloc, AuthState>(
+      listener: (context, state) async {
+        if (state is AuthStateLoggedOut) {
+          // if (state.exception is UserNotFoundAuthException) {
+          //   await showErrorDialog(context, 'User not found');
+          // } else if (state.exception is WrongPasswordAuthException) {
+          //   await showErrorDialog(context, 'Wrong credentials');
+          // } else if (state.exception is GenericAuthException) {
+          //   await showErrorDialog(context, 'Authentication error');
+          // }
+        }
+      },
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Center(
+          child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(top: 75),
+                child: const Text(
+                  'Log-in',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 35,
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 35),
+                child: SizedBox(
+                  width: 300,
+                  height: 50,
+                  child: TextField(
+                    controller: emailController,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      labelText: 'Email',
+                      labelStyle: TextStyle(
+                        fontSize: 15,
+                        color: Color(0xFF000000),
+                      ),
+                      enabledBorder: OutlineInputBorder(),
+                      focusedBorder: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 35),
+                child: SizedBox(
+                  width: 300,
+                  height: 50,
+                  child: TextField(
+                    controller: passController,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                      labelStyle: TextStyle(
+                        fontSize: 15,
+                        color: Color(0xFF000000),
+                      ),
+                      enabledBorder: OutlineInputBorder(),
+                      focusedBorder: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 35),
+                child: SizedBox(
+                  width: 150,
+                  child: TextButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(secondaryAppColor),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                        ),
+                      ),
+                    ),
+                    child: const Text(
+                      'Log in',
+                      style: TextStyle(fontSize: 17),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 10),
+                child: SizedBox(
+                  width: 150,
+                  child: TextButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(secondaryAppColor),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                        ),
+                      ),
+                    ),
+                    child: const Text(
+                      'Sign in',
+                      style: TextStyle(fontSize: 17),
+                    ),
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 10),
+                      child: const Divider(
+                        color: Colors.black,
+                        thickness: 1,
+                      ),
+                    ),
+                  ),
+                  const Text('OR'),
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 10),
+                      child: const Divider(
+                        color: Colors.black,
+                        thickness: 1,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 10),
+                child: SizedBox(
+                  width: 250,
+                  child: TextButton.icon(
+                    onPressed: () {},
+                    icon: const ImageIcon(
+                        AssetImage('assets/images/google_img.png')),
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(secondaryAppColor),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                        ),
+                      ),
+                    ),
+                    label: const Text(
+                      'Sign in with Google',
+                      style: TextStyle(fontSize: 17),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 20),
+                child: Image.asset(
+                  'assets/images/earth.png',
+                  cacheHeight: 130,
+                  cacheWidth: 130,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
